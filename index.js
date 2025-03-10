222be7a4c0c3f23e7fa0e8373850c351636adfc1
loisina <adjoaasiedu4@gmail.com>
2025-03-10 15:19:04 +0000

    added the validators for the library api

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+// make database 
+await mongoose.connect(process.env.MONGO_URI);
+
+ 
+// create an express app
+const app = express();
+// Use global middle wares
+app.use (express.json())
+
+// use routes
+app.use(Router);
+
+
+
+// Listen for incoming request
+
+app.listen(3000, ()=>{
+  console.log('Server listening on port 3000')
+})
